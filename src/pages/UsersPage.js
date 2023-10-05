ed7f2ac7384bd7ff88518aa3eca91919104224dd
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2023-10-04 21:05:36 -0300

    cree app de usuarios y linkie bootstrap

---
+++ src/pages/UsersPage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const UsersPage = () => {
+  return (
+    <div className="mt-5">
+        <h1>Users</h1>
+        <hr/>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Avatar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>ID</td>
+                    <td>Email</td>
+                    <td>Name</td>
+                    <td>Avatar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
